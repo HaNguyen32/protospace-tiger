da2534e4a27460f62343e72f105e031d450500db
HaNguyen32 <juhi_nguyen.hfl@outlook.jp>
2023-07-19 21:24:06 +0900

    カラム追加

--- db/migrate/20230719103531_create_prototypes.rb
+++ db/migrate/20230719103531_create_prototypes.rb
@@ -1,8 +1,9 @@
 class CreatePrototypes < ActiveRecord::Migration[7.0]
   def change
     create_table :prototypes do |t|
-      
-
+      t.string :title
+      t.text :catch_copy, null: false
+      t.text :concept, null: false
       t.timestamps
     end
   end
